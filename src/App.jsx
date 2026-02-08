6328bd4938bf801e8007c79f53a03d7b7c925f5f
Sergei <goldengrail@gmail.com>
2026-02-08 14:24:43 +0000

    video 3 part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+const username = 'Sergei'
+
 const App = () => {
     return (
         <div>
-            <h1>To Do List</h1>
+            <h1 className="title">To Do List</h1>
+            <p style={{color: 'red', fontWeight: 700}}>Hi, {username}</p>
+            <p>{1+1}</p>
+            <p>{username.toUpperCase()}</p>
+            <p>{new Date().toLocaleDateString()}</p>
+            {/*comment inside jsx*/}
+            <hr />
+            <label htmlFor="email">Email:</label>
+            <input type="email" required={true} />
+            <input type="email" required />
         </div>
     )
 }
