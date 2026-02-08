2c480c6bc4ded81ca8dee221f0eef5ac9eb9f89b
Sergei <goldengrail@gmail.com>
2026-02-08 15:13:17 +0000

    video 3 part 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-const username = 'Sergei'
-const isLoggedIn = false
-
 const App = () => {
-    let content
-
-    if (isLoggedIn) {
-        content = <p>Hi, {username}!</p>
-    } else {
-        content = <button>Log in</button>
-    }
-
+    const tasks = [
+        'Feed cat',
+        'Make cofee',
+        'Watch TV',
+    ]
     return (
        <>
             <h1 className="title">To Do List</h1>
-            <p>{isLoggedIn ? 'Hi, ${username}!' : 'Please log in'}</p>
-            {isLoggedIn && <p>Hi, {username}</p>}
-            {isLoggedIn ? <p>Hi, {username}!</p> : <button>Log in</button>}
-            {content}
+            <ul>
+                {tasks.map((task) => <li key={task}>{task}</li>)}
+            </ul>
        </>
     )
 }
